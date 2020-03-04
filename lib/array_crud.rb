9e4ce8a65ea346411de142423608195bb6e4c9b6
Elvis Ning <ningelvis@gmail.com>
2020-03-04 22:12:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["Blue", "Red", "White", "Black"]
+  array = ["wow", "Red", "White", "Black"]
   element = "arrays!"
   array.unshift
 end
